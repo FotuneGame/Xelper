472f67131d103cee5782b6413e1b031caba6cf23
Victory-wvw <volkv_v@bk.ru>
2024-04-22 21:26:26 +0300

    .

--- web/services/event/src/page/components/event/Event.tsx
+++ web/services/event/src/page/components/event/Event.tsx
@@ -2,6 +2,38 @@ import React from 'react';
 import "./Event.module.scss";
 import {useParams} from "react-router-dom";
 
+/*
+interface EventCardProps {
+    title: string;
+    description: string;
+    image: string;
+    date: string;
+    location: string;
+    link: string;
+}
+
+const EventCard: React.FC<EventCardProps> = ({ title, description, image, date, location, link }) => {
+    return (
+        <div className={styles.eventCard}>
+            <div className={styles.imageWrapper}>
+                <img src={image} alt={title} className={styles.image} />
+            </div>
+            <div className={styles.content}>
+                <div className={styles.title}>{title}</div>
+                <div className={styles.description}>{description}</div>
+                <div className={styles.dateLocation}>
+                    <span>{date}</span>
+                    <span>{location}</span>
+                </div>
+                <a href={link} className={styles.link}>Learn more</a>
+            </div>
+        </div>
+    );
+};
+
+export default EventCard;
+*/
+
 
 const Event = () => {
     const params = useParams();
